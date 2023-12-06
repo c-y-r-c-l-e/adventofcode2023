b5cff5c5b2e93896334335c493703f4115bc578b
Cyril Cleven <cyril@cyrcle.nl>
2023-12-06 17:03:16 +0100

    day 1

--- 1.py
+++ 1.py
@@ -1,1 +1,37 @@
-aaaaaaaaaaaaaaaaaaa
+# On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+#
+# For example:
+#
+# 1abc2
+# pqr3stu8vwx
+# a1b2c3d4e5f
+# treb7uchet
+#
+# In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+#
+# Consider your entire calibration document. What is the sum of all of the calibration values?
+
+import re
+
+filename = "/Users/cyril/Documents/aoc23/eightfivesssxxmgthreethreeone1sevenhnz.txt"
+
+# with open(filename) as file:
+#     for line in file:
+#         print('\n')
+#         print(line)
+#         numbers = re.findall(r'\d', line)
+#         print(numbers)
+#         selectednumbers = numbers[0] + numbers[-1]
+#         print(selectednumbers)
+#         int(selectednumbers)
+
+def getcalibrationvalues(line):
+    numbers = re.findall(r'\d', line)
+    selectednumbers = numbers[0] + numbers[-1]
+    result = int(selectednumbers)
+    return(result)
+
+calibrationvalues = [getcalibrationvalues(line) for line in open(filename)]
+print(calibrationvalues)
+total = sum(calibrationvalues)
+print("Sum of calibrationvalues:  " + str(total))
